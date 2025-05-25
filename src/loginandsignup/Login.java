c0d1de061ee4bd35f10c8600b918265b17ec71a1
andre <andre@192.168.1.101>
2025-05-25 14:47:28 +0200

    IceTask3

--- src/loginandsignup/Login.java
+++ src/loginandsignup/Login.java
@@ -10,7 +10,7 @@ import javax.swing.JOptionPane;
 import java.awt.HeadlessException;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
-import java.awt.Window
+import java.awt.Window;
 
 public class Login extends javax.swing.JFrame {
 
